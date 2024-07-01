40da37551aee806e7bedb206e4adca4b6235da00
tlambertnerf <86309648+tlambertnerf@users.noreply.github.com>
2024-07-01 14:09:36 +0200

    Add files via upload

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 #html_theme = 'alabaster'
-html_theme = "sphinx_book_theme"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
 
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']
